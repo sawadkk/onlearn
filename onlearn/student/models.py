2dcb9b84634d2e1a7a6bf66ff93fac82f882aff2
sawadkk <60354165+sawadkk@users.noreply.github.com>
2020-11-15 09:24:20 +0530

    Update models.py

--- onlearn/student/models.py
+++ onlearn/student/models.py
@@ -3,6 +3,3 @@ from django.contrib.auth.models import User
 from teacher.models import *
 # Create your models here.
 
-class mycourses(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
