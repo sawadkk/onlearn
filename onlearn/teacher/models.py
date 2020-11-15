f23647b5cf997f2045124f831b1463dab351086f
sawadkk <60354165+sawadkk@users.noreply.github.com>
2020-11-15 09:25:36 +0530

    Update models.py

--- onlearn/teacher/models.py
+++ onlearn/teacher/models.py
@@ -67,3 +67,7 @@ class Quiz(models.Model):
     choice_four = models.CharField(max_length = 200)
     choice_five = models.CharField(max_length = 200)
     answer = models.CharField(max_length = 200)
+    
+  class mycourses(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
